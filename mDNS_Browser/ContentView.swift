daadff659df9e1ecd04854f8c8586ab29f9b743b
Mark Robberts <robberts75@gmail.com>
2021-07-21 17:23:35 +0200

    Initial Commit

---
+++ mDNS_Browser/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  mDNS_Browser
+//
+//  Created by Mark Robberts on 2021/07/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ mDNS_Browser/mDNS_BrowserApp.swift
@@ -0,0 +1,17 @@
+//
+//  mDNS_BrowserApp.swift
+//  mDNS_Browser
+//
+//  Created by Mark Robberts on 2021/07/21.
+//
+
+import SwiftUI
+
+@main
+struct mDNS_BrowserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
