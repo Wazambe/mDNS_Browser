f51eb92a02681aba24ed974397f95d45f26bb711
Mark Robberts <robberts75@gmail.com>
2021-07-22 14:08:59 +0200

    NWBrowser Bonjour example

--- mDNS_Browser/ContentView.swift
+++ mDNS_Browser/ContentView.swift
@@ -3,14 +3,74 @@
 //  mDNS_Browser
 //
 //  Created by Mark Robberts on 2021/07/21.
-//
+
+//  Check out: https://developer.apple.com/library/archive/documentation/Cocoa/Conceptual/NetServices/Articles/NetServicesArchitecture.html
+
+//Check out: https://developer.apple.com/news/?id=0oi77447
+
+//Add to Info.plist: Privacy -> Local Network Usage Description
+//Add to Info.plist: Bonjour services Item 0 ->    _mytype._tcp
+//  (obviously the actual service type that one would be using)
 
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var serviceType   = "_mytype._tcp"
+    @State private var netDomain = "local."
+    @State private var isSelected = false
+    @ObservedObject var bonBrowser = mCastBrowser()
+       
+    
     var body: some View {
-        Text("Hello, world!")
+        VStack {
+            Text("Scan the local network, and list all devices of given type")
+                .font(.caption)
+                .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
+            HStack {
+                VStack(alignment: .trailing) {
+                    Text("Service Type ")
+                    Text("Domain ")
+                        .padding(.top, 2)
+                }
+                .foregroundColor(.gray)
+                VStack(alignment: .center) {
+                    ///Service type in the format - underscore name dot underscore protocol -
+                    ///should one want to scan all, then it seems that special entitlment needs to be obtained from Apple
+                    ///For testing one can use a Bonjour browser tool, and use service type from one of the availables e.g. _rdlink._tcp
+                    TextField("Service Type", text: $serviceType)
+                        .border(Color.gray)
+                    ///Almost always just local.
+                    ///Going ouside of the local is a whole other story
+                    TextField("Domain", text: $netDomain)
+                        .border(Color.gray)
+                }
+            }
             .padding()
+            
+            Button(action: {
+                self.bonBrowser.scan(typeOf: serviceType, domain: netDomain)
+//                self.doTCP.resetTCPLink()
+            }, label: {
+                Text("Search devices")
+            })
+            .padding(.bottom, 32)
+            .colorInvert()
+            
+            List {
+              ForEach(bonBrowser.devices, id: \.self) { each in
+                      Text(each.device)
+                      .onTapGesture {
+//                        self.name = each.device
+                        self.isSelected.toggle()
+                }
+                  }
+      //            .font(Fonts.avenirNextCondensedBold(size: 16))
+              .listRowBackground(isSelected ? Color(#colorLiteral(red: 0.9686274529, green: 0.78039217, blue: 0.3450980484, alpha: 1)): Color.clear)
+              }
+            
+        }
+        .padding()
     }
 }
 
---
+++ mDNS_Browser/mCastBrowser.swift
@@ -0,0 +1,91 @@
+//
+//  mCastBrowser.swift
+//  mDNS_Browser
+//
+//  Created by Mark Robberts on 2021/07/22.
+//
+
+import UIKit
+import Combine
+import Network
+
+class mCastBrowser: NSObject, ObservableObject, Identifiable {
+  
+  struct objectOf:Hashable {
+     var id:UUID? = UUID()
+     var device:String = ""
+     var IsIndexed:Int = 0
+  }
+  
+  @Published var devices: [objectOf] = [] {
+    willSet {
+      objectWillChange.send()
+    }
+  }
+  
+  var browser: NWBrowser!
+      
+    func scan(typeOf: String, domain: String) {
+    ///Primarily use the Network Browser with the Bonjour descriptor
+    let bonjourTCP = NWBrowser.Descriptor.bonjour(type: typeOf , domain: domain)
+    
+    let bonjourParms = NWParameters.init()
+        bonjourParms.allowLocalEndpointReuse = true
+        bonjourParms.acceptLocalOnly = true
+        bonjourParms.allowFastOpen = true
+    
+    browser = NWBrowser(for: bonjourTCP, using: bonjourParms)
+    browser.stateUpdateHandler = {newState in
+      switch newState {
+      case .failed(let error):
+        print("NW Browser: now in Error state: \(error)")
+        self.browser.cancel()
+      case .ready:
+        print("NW Browser: new bonjour discovery - ready")
+      case .setup:
+        print("NW Browser: ooh, apparently in SETUP state")
+      default:
+        break
+      }
+    }
+    browser.browseResultsChangedHandler = { ( results, changes ) in
+        print("NW Browser: Scan results found:")
+      for result in results {
+        print(result.endpoint.debugDescription)
+        }
+      for change in changes {
+//        print(change.hashValue)
+        if case .added(let added) = change {
+            print("NW Browser: Added")
+//            case service(name: String, type: String, domain: String, interface: NWInterface?)
+///         This is the interesting part - the service has 4 parts - almost matching endpoint
+///         because the endpoint has name Type, Domain, but then also metadata, and the last match with the service; interface
+//          if case .service(let name, let type, let domain, let interface) = added.endpoint {
+///         The real question should be, why not just create an array of endpoints, and keep that as reference?
+///         But that  we can do when connecting with endpoint name, because then it is best to refresh
+            if case .service(let name, _, _, _) = added.endpoint {
+//            let device = objectOf(device: service.name, IsIndexed: self.devices.count)
+                let device = objectOf(device: name, IsIndexed: self.devices.count)
+//            self.devices.removeAll()
+            self.devices.append(device)
+          }
+            
+          if case .removed(let removed) = change {
+            print("NW Browser: Removed")
+            if case .service(let name, _, _, _) = removed.endpoint {
+              let index = self.devices.firstIndex(where:{$0.device == name })
+              self.devices.remove(at: index!)
+            }
+          }
+        }
+      }
+    }
+    self.browser.start(queue: DispatchQueue.main)
+  }
+    
+    
+    
+    
+}
+
+
